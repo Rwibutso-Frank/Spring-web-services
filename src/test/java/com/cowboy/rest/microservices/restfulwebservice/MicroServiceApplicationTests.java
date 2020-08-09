5c8c870f65db1f5ad82c055a7f1a63a41a5c1777
Rwibutso Frank <rwibufranko250@gmail.com>
2020-08-09 10:13:52 +0200

    initial commit

---
+++ src/test/java/com/cowboy/rest/microservices/restfulwebservice/MicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowboy.rest.microservices.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
